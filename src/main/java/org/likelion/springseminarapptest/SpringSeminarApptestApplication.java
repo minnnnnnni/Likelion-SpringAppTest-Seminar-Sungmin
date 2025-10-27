cd935fc04075f6faeec0e6c8ba8e4117398b458a
Chaemin Yu <chaemin.u@gmail.com>
2025-10-27 19:39:05 +0900

    Initial Commit

---
+++ src/main/java/org/likelion/springseminarapptest/SpringSeminarApptestApplication.java
@@ -0,0 +1,13 @@
+package org.likelion.springseminarapptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSeminarApptestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSeminarApptestApplication.class, args);
+    }
+
+}
